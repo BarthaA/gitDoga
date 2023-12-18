284b996edfe3f8952684fa17593b53010c189792
Bartha Álmos <bartha.almos0306@gmail.com>
2023-12-18 12:51:43 +0100

    3. feladat

--- gitCommitok/Program.cs
+++ gitCommitok/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
-
-namespace gitCommitok
+﻿namespace gitCommitok
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> Lista = new List<int>();
+            Console.WriteLine("Kérek egy számot: ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            while (szam != 0)
+            {
+                Lista.Add(szam);
+                foreach (var item in Lista)
+                {
+                    Console.Write(item);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Kérek egy számot: ");
+                szam = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.ReadKey();
         }
     }
 }
